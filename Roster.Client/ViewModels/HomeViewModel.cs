c1e2eb0d9e39d53f0a7c41dbe2b71c7e452d2a12
Amber Metini <amberella1981@yahoo.com>
2022-01-27 11:04:29 +0100

    Completed mod.3

---
+++ Roster.Client/ViewModels/HomeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Roster.Client.ViewModels
+{
+    public class HomeViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Title { get; set; } = "Roster App";
+        public Command UpdateApplicationCommand { get; }
+    }
+}
